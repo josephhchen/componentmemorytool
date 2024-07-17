878d31ac7e8e9d52f43cddb085e2a4ad5dcd8a14
Joseph Chen <joehaiyuechen@gmail.com>
2024-07-17 15:23:38 -0400

    Code for demo

---
+++ ios/MemoryUsage.h
@@ -0,0 +1,12 @@
+//
+//  MemoryUsage.h
+//  ComponentMemoryTool
+//
+//  Created by Joseph Chen on 7/17/24.
+//
+
+#ifndef MemoryUsage_h
+#define MemoryUsage_h
+
+
+#endif /* MemoryUsage_h */
---
+++ ios/MemoryUsage.m
@@ -0,0 +1,8 @@
+//
+//  MemoryUsage.m
+//  ComponentMemoryTool
+//
+//  Created by Joseph Chen on 7/17/24.
+//
+
+#import <Foundation/Foundation.h>
